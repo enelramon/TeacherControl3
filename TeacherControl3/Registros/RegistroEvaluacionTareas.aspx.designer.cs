6c7ae04c3fe3968a65c4e0f93c0026c42ea74fa0
Enel Almonte <enelramon@gmail.com>
2015-02-11 20:14:48 -0400

    Actualizando correcciones en clase

--- TeacherControl3/Registros/RegistroEvaluacionTareas.aspx.designer.cs
+++ TeacherControl3/Registros/RegistroEvaluacionTareas.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace TeacherControl3.Registros {
         protected global::System.Web.UI.WebControls.Label Label2;
         
         /// <summary>
+        /// VerEstudianteButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button VerEstudianteButton;
+        
+        /// <summary>
         /// IdEstudianteTextBox control.
         /// </summary>
         /// <remarks>
