b2f53104a869a7cf0188c9e899a6136296b7b3b1
Enel Almonte <enelramon@gmail.com>
2015-02-18 21:04:15 -0400

    Agregando Semestres

--- DAL/ConexionDb.cs
+++ DAL/ConexionDb.cs
@@ -18,8 +18,8 @@ namespace DAL
         //    get { return WebConfigurationManager.AppSettings["ConexionString"]; }
         //}
 
-        public static SqlConnection con = new SqlConnection(@"Data Source=.\SqlExpress;Initial Catalog=TeacherControlDb;Integrated Security=True");
-        //public static SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\TeacherControlDb.mdf;Integrated Security=True;Connect Timeout=30");
+        //public static SqlConnection con = new SqlConnection(@"Data Source=.\SqlExpress;Initial Catalog=TeacherControlDb;Integrated Security=True");
+        public static SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\TeacherControlDb.mdf;Integrated Security=True;Connect Timeout=30");
 
         public string ultimoError;
 
