7f54b927e76efe32a8c9c213c0376b1749348c05
Enel Almonte <enelramon@gmail.com>
2015-02-27 12:17:01 -0400

    Agregando BllTest y arreglando maste page

---
+++ BLLTests/UsuariosTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class UsuariosTests
+    {
+        [TestMethod()]
+        public void UsuariosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertarTest()
+        {
+            bool paso =false;
+
+
+
+            Assert.AreEqual(paso, true, "Inserto");
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AutenticarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
